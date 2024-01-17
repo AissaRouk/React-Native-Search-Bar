9ccd85177f0753e7c12696763409834864ad7ff9
Aissa Rouk <aissa.rouk@gmail.com>
2024-01-17 13:01:46 +0200

    Main App file

---
+++ Akcijos.js
@@ -0,0 +1,108 @@
+const Akcijos = [
+  {
+    id: 1,
+    name: "Plautos morkos CLEVER",
+    quantity: 1,
+    quantityType: 5,
+    discountPrice: 0.59,
+    isCardDiscounted: true,
+    price: 1.49,
+    image:
+      "https://storage.googleapis.com/download/storage/v1/b/lastmile-ui/o/images%2Fproducts%2Fthumb_0SFs93BQwFn5k23gDvll.jpg?generation=1631004429577083&alt=media",
+    soldBy: 2,
+    store: 1,
+  },
+  {
+    name: "Sveriamos bulvės CLEVER",
+    quantity: 1,
+    quantityType: 2,
+    discountPrice: 0.49,
+    isCardDiscounted: true,
+    price: 1.29,
+    image:
+      "https://storage.googleapis.com/download/storage/v1/b/lastmile-ui/o/images%2Fproducts%2Fthumb_zlyd0ZGFLdmyu8brBFh2.jpg?generation=1582208339280093&alt=media",
+    soldBy: 2,
+    id: 2,
+    store: 1,
+  },
+  {
+    name: "Svogūnai CLEVER",
+    quantity: 1,
+    quantityType: 2,
+    discountPrice: 0.69,
+    isCardDiscounted: false,
+    price: 1.39,
+    image:
+      "https://storage.googleapis.com/download/storage/v1/b/lastmile-ui/o/images%2Fproducts%2Fthumb_BoT2PeAAz5J3AuHCTyte.jpg?generation=1639147461726135&alt=media",
+    soldBy: 2,
+    id: 3,
+    store: 1,
+  },
+  {
+    name: "Citrinos",
+    quantity: 1,
+    quantityType: 2,
+    discountPrice: 1.69,
+    isCardDiscounted: false,
+    price: 2.49,
+    image:
+      "https://storage.googleapis.com/download/storage/v1/b/lastmile-ui/o/images%2Fproducts%2Fthumb_H83PY5twSuOZBcPevKFy.jpg?generation=1631695789675754&alt=media",
+    soldBy: 2,
+    id: 4,
+    store: 1,
+  },
+  {
+    name: "Mandarinai",
+    quantity: 1,
+    quantityType: 2,
+    discountPrice: 1.49,
+    isCardDiscounted: true,
+    price: 2.99,
+    image:
+      "https://storage.googleapis.com/download/storage/v1/b/lastmile-ui/o/images%2Fproducts%2Fthumb_Hy7HIb8PQCGqEvATZw8b.jpg?generation=1631695783290806&alt=media",
+    soldBy: 2,
+    id: 5,
+    store: 1,
+  },
+  {
+    name: "Sveriami obuoliai ŠAMPION",
+    quantity: 1,
+    quantityType: 2,
+    discountPrice: 0.69,
+    isCardDiscounted: false,
+    price: 0.99,
+    image:
+      "https://storage.googleapis.com/download/storage/v1/b/lastmile-ui/o/images%2Fproducts%2Fthumb_hEyrjLkWqfDN7Ce8b3K5.jpg?generation=1589636330241009&alt=media",
+    soldBy: 2,
+    id: 6,
+    store: 1,
+  },
+  {
+    name: "Besėklės žaliosios vynuogės",
+    quantity: 500,
+    quantityType: 1,
+    discountPrice: 1.49,
+    isCardDiscounted: false,
+    price: 2.49,
+    image:
+      "https://storage.googleapis.com/download/storage/v1/b/lastmile-ui/o/images%2Fchain_products%2FIKI%2Fthumb_hcmNGxBKsp0jcdhciqfK.jpg?generation=1654374772066054&alt=media",
+    soldBy: 5,
+    id: 7,
+    store: 1,
+  },
+  {
+    name: "Didžiosios conference kriaušės",
+    quantity: 1,
+    quantityType: 2,
+    discountPrice: 2.39,
+    isCardDiscounted: false,
+    price: 2.99,
+    image:
+      "https://storage.googleapis.com/download/storage/v1/b/lastmile-ui/o/images%2Fproducts%2Fthumb_oCU2QwVelSeMv0gjLqmB.jpg?generation=1654689194993011&alt=media",
+    soldBy: 2,
+    id: 8,
+    store: 1,
+  },
+];
+
+export default Akcijos;
